e4a5c54faa8c411a85ba3fbf859d19b2c54a0ce6
Justin Barca <4888331+justin-hackin@users.noreply.github.com>
2024-03-30 17:11:52 -0400

    remix init, use generated app names

---
+++ scripts/fly-create.sh
@@ -0,0 +1,8 @@
+fly apps create test-indie-stack-2b2d
+fly apps create test-indie-stack-2b2d-staging
+
+fly volumes create data --size 1 --app test-indie-stack-2b2d
+fly volumes create data --size 1 --app test-indie-stack-2b2d-staging
+
+fly secrets set SESSION_SECRET=$(openssl rand -hex 32) --app test-indie-stack-2b2d
+fly secrets set SESSION_SECRET=$(openssl rand -hex 32) --app test-indie-stack-2b2d-staging
