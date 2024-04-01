c0cd428c2e343417f2a065c1c15cf5edd5d22004
Justin Barca <4888331+justin-hackin@users.noreply.github.com>
2024-04-01 10:18:20 -0400

    retrigger deploy after adding SESSION_SECRET

--- scripts/fly-create.sh
+++ scripts/fly-create.sh
@@ -6,3 +6,4 @@ fly volumes create data --size 1 --app test-indie-stack-2b2d-staging
 
 fly secrets set SESSION_SECRET=$(openssl rand -hex 32) --app test-indie-stack-2b2d
 fly secrets set SESSION_SECRET=$(openssl rand -hex 32) --app test-indie-stack-2b2d-staging
+
